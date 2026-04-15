3a3841ae54561993bf31d20c3a89a0961e1354e2
Qadir Bux <166857064+qadir1991@users.noreply.github.com>
2026-04-15 17:28:56 +0500

    Create using a break statement.py

---
+++ using a break statement.py
@@ -0,0 +1,9 @@
+# using a infinite loop
+while True:
+  print("write correct name")
+  name=input()
+  # using s condition
+  if name=="qadir":
+    #break statement is used to stop the loop, while condition is True
+    break
+print("thanks")
